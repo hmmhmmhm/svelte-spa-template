64c030d2ff442ce745313ca9d8894a45217918b2
hmmhmmhm <hmmhmmhm@naver.com>
2019-12-07 11:34:33 +0900

    Add More Description in REST API

--- src/logics/api/restapi.ts
+++ src/logics/api/restapi.ts
@@ -1,11 +1,22 @@
 import {
     RestAPI,
     getToken
-} from '../../reuseable/logics'
+} from '../../utils'
 
 export const BackEndAPI = new RestAPI({
+
+    // Type the Back-End API Server address.
     address: 'http://localhost',
+
+    // Enable this option if you use an
+    // authentication token for API connection.
     isUseAuth: true,
+
+    // Enter the token (string) if
+    // want to use when the API connection.
     getToken,
+
+    // Specifies the function that will run
+    // if an error occurs during communication.
     faultTolerance: (e)=> console.log(e),
 })
