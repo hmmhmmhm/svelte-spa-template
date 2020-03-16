fbe429d6a3906ffd609b50189cb4a2d583b11676
hmmhmmhm <hmmhmmhm@naver.com>
2020-03-16 09:21:45 +0900

    Update Svelte 3.20.0

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
+import * as ReusableComponents from 'svelte-reusable/components'
+
 // @ts-ignore
 import Template from './_Template.svelte'
-
 export const Components = {
+    ...ReusableComponents,
     Template,
 }
